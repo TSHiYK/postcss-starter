1a34c04af2c1b39fd5743625220cd26c36f1cb26
TSHiYK <toshiyuki.i1223+dev@gmail.com>
2016-10-27 22:17:00 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  entry: {
+    main: './src/main.ts'
+  },
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'src',
+    port: 8080
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts', '.css', '.pcss']
+  },
+  module: {
+    loaders: [
+      {
+          test: /\.ts$/,
+          loaders: ['awesome-typescript-loader']
+      },
+      {
+        test: /\.pcss$/,
+        loaders: ['style-loader', 'css-loader?importLoaders=1', 'postcss-loader']
+      },
+    ]
+  },
+  postcss: function () {
+    return [
+      require('precss')({ /* ...options */ }),
+      require('autoprefixer')({
+        browsers: ['last 3 versions', 'ie 9', 'ios 6', 'android 4.4']
+      })
+    ];
+  }
+}
